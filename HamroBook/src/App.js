a91eb018b6072e87c5e63b8b544ff657549c6225
lizanbajracharya <lizan.bajra97@gmail.com>
2020-02-03 20:38:56 +0545

    design

--- HamroBook/src/App.js
+++ HamroBook/src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HeaderComponent from './components/HeaderComponent';
+import BodyComponent from './components/BodyComponent';
+import FooterComponent from './components/FooterComponent';
+import LoginComponent from './components/login/LoginComponent';
 
 function App() {
   return (
     <div className="App">
       <HeaderComponent/>
+      <BodyComponent/>
+      <FooterComponent/>
     </div>
   );
 }
