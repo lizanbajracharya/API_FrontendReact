4de4692abbee0b40bbaa8333d1acd54e27ec3c23
lizanbajracharya <lizan.bajra97@gmail.com>
2020-02-11 17:56:11 +0545

    Login and register done

--- HamroBook/src/App.js
+++ HamroBook/src/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-import {Route, BrowserRouter} from 'react-router-dom'
-import AdminPanel from './components/admin/AdminPanel';
-import HeaderComponent from './components/HeaderComponent';
-import BodyComponent from './components/BodyComponent';
-import FooterComponent from './components/FooterComponent';
+import { Container } from 'reactstrap';
+import PrivateRoute from './components/utils/PrivateRoute';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import LoginComponent from './components/LoginComponent';
+import NoMatch from './components/NoMatch';
+import Register from './components/Register'
+import AdminDashboard from './components/admin/AdminPanel';
+import Admin from './components/Admin';
+import Dashboard from './components/Dashboard';
+import UserProfile from './components/UserProfile';
 function App() {
   return (
-    <div className="App">
-      <HeaderComponent/>
-     <BodyComponent/>
-     <FooterComponent/>
-    </div>
+    <Container>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={LoginComponent} />
+        <Route path='/register' component={Register} />
+        <Route path='/admin' component={Admin}/>
+        <PrivateRoute path='/admindashboard' component={AdminDashboard} />
+        <PrivateRoute path='/dashboard' component={Dashboard} />
+        <PrivateRoute path='/profile' component={UserProfile} />
+        <Route>
+          <NoMatch />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+
+  </Container>
   );
 }
 
